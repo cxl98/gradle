e07e2ba02cb7bd4665d668db0db7dd8f5d0e23ce
cxl <1799359457@qq.com>
2020-12-23 19:36:36 +0800

    entity

--- web/src/main/java/com/easyarch/service/AdminService.java
+++
@@ -1,8 +0,0 @@
-package com.easyarch.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class AdminService {
-
-}
--- web/src/main/java/com/easyarch/service/QuestionService.java
+++ web/src/main/java/com/easyarch/service/QuestionService.java
@@ -1,8 +1,22 @@
 package com.easyarch.service;
 
+import com.easyarch.entity.Question;
+import com.easyarch.mongo.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class QuestionService {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    public List<Question> showQuestion(int curr,int pageSize){
+        int firstIndex = (curr - 1) * pageSize;
+        int lastIndex = curr * pageSize;
+        return questionRepository.findAll().subList(firstIndex,lastIndex);
+    }
 
 }
