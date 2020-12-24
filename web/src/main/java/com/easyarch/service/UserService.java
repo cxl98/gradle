d71d2b108223def837c85ab4846890310aa94c63
cxl <1799359457@qq.com>
2020-12-24 21:01:20 +0800

    entity

--- web/src/main/java/com/easyarch/service/UserService.java
+++ web/src/main/java/com/easyarch/service/UserService.java
@@ -1,9 +1,40 @@
 package com.easyarch.service;
 
+import com.easyarch.dao.UserDao;
+import com.easyarch.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
 @Service
 public class UserService {
 
+    @Autowired
+    private UserDao userDao;
+
+
+    public User login(String username,String password){
+        if (null == username && null == password){
+            return null;
+        }
+        return userDao.findByNameAndPassword(username, DigestUtils.md5DigestAsHex(password.getBytes()));
+    }
+
+    public User isFinished(String id){
+        if(null!=id){
+           return userDao.isFinished(id);
+        }
+        return  null;
+    }
+
+    public int addUser(User user){
+        int i = userDao.insertUser(user);
+        if (i!=0){
+            return 1;
+        }
+        return 0;
+    }
+    public User findByUsername(String username){
+        return userDao.findByName(username);
+    }
 }
